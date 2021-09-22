addd89103aeff0807b3ca4e9f53a70c38092d30a
kealva <alvkev336@gmail.com>
2021-09-21 20:43:59 -0400

    Final Product for HW4

---
+++ app/public/js/index.js
@@ -0,0 +1,37 @@
+
+const SomeApp = {
+    data() {
+      return {
+        "dataP": {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.dataP.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.dataP = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        return this.fetchUserData();
+    }
+
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
