aaab3e3933a694273b9ed94b7328974db5bcf213
kealva <alvkev336@gmail.com>
2021-09-29 10:24:59 -0400

    php

---
+++ app/public/api/test.php
@@ -0,0 +1,31 @@
+<?php
+
+$num = 2;
+$foo = $num . " be ":
+$bar = "or not" .$num. " be";
+
+echo $foo . ' ' . $bar;
+
+echo $num * $num * $num;
+
+$arr = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS:"
+];
+
+if (ture) {
+    echo "\nTRUE\n":
+}
+
+while (true) {
+    //This way it doesnot actually do anything
+    break;
+}
+
+foreach($i:arr) {
+    echo $i;
+}
+
+echo "<ul>";
+foreach($arr as $key)
